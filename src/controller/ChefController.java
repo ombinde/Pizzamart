be60792a7fcf741c6961011cda8adb0ed7209623
sigurlu <sigurd@sigurd-laptop.(none)>
2011-10-21 22:45:45 +0200

    Kommentert litt og gjort ferdig metoden getFreshOrders

--- src/controller/ChefController.java
+++ src/controller/ChefController.java
@@ -7,30 +7,12 @@ import java.util.HashMap;
 
 import model.*;
 public class ChefController {
-	
-	//helpe metode til
-	private ArrayList<Integer> getOrdersById(){
-		ArrayList<Integer> freshOrders = new ArrayList<Integer>();
-		try {
-			Database db;
-			db = Database.getDatabase();
-			ResultSet rs = db.select("SELECT * FROM orders WHERE status='Bestilt'");
-			while(rs.next()){
-				freshOrders.add(rs.getInt("idorder"));
-			}
-			return freshOrders;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
+		
 	public ArrayList<HashMap<Product,Integer>> getFreshOrders(){
 		ArrayList<HashMap<Product,Integer>> freshOrders= new ArrayList<HashMap<Product, Integer>>();
-		ArrayList<Integer> idOrders = getOrdersById();
+		ArrayList<Integer> idOrders = Order.getOrdersById("Bestilt");
 		for(int i=0; i<idOrders.size(); i++){
-			Order.getProductsFromOrdre(idOrders.get(i));
+			freshOrders.add(Order.getProductsFromOrdre(idOrders.get(i)));
 		}
 		return freshOrders;
 	}
