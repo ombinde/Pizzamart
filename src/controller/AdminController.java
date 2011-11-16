d64f4dd95e3a07e1ada48743359191610f072df1
ombinde <oivindbi@stud.ntnu.no>
2011-11-16 17:50:14 +0100

    javadoc and library cleanup

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -19,11 +19,10 @@ public class AdminController {
 	/**
 	 * Adds a product to the database.
 	 * Returns true if the product is added, and false if something went wrong.
-	 * If the product isn't added it's most likely already exist.
-	 * @param name
-	 * @param price
-	 * @param comment
-	 * @return
+	 * @param name product name
+	 * @param price product price
+	 * @param comment product comment
+	 * @return true if the product is added, and false if something went wrong.
 	 */
 	public static boolean addProduct(String name, double price, String comment){
 		return Product.addProductToDatabase(name, price, comment);
@@ -31,9 +30,8 @@ public class AdminController {
 	
 	/**
 	 * Deletes a product from the database.
-	 * Returns true if the product was deleted, else it's false.
-	 * @param p
-	 * @return
+	 * @param p is product 
+	 * @return true if the product was deleted, else it's false.
 	 */
 	public static boolean deleteProduct(Product p){
 		return Product.deleteProductFromDatabase(p);
@@ -41,31 +39,33 @@ public class AdminController {
 	
 	/**
 	 * Returns all the products from the database.
-	 * @return
+	 * @param query is string that should specify a productname
+	 * @return arraylist of productnames that match the search string
 	 */
 	public static ArrayList<Product> getRelevantProducts(String query){
 		return Product.getRelevantProducts(query);
 	}
 	
 	/**
-	 * Updates a product p in the database with the given parameters.
-	 * Returns true if the product was updated successfully. If false something wrong happend, 
-	 * and it wasn't updated.
-	 * @param p
-	 * @param name
-	 * @param price
-	 * @param comment
-	 * @return
-	 */
+	 * Updates a product p in the database with the given parameters. 
+	 * @param p is product
+	 * @param name is product name
+	 * @param price is product price
+	 * @param comment is product description
+	 * @return true if the product was updated successfully. And false if something unexpected happened.
 	public static boolean updateProduct(Product p, String name, double price, String comment){
 		return Product.updateProduct(p, name, price, comment);
 	}
 	
 	/**
 	 * Sets the limit for when delivery is going to be free.
-	 * Returns true if the value was updated, and false if not.
-	 * @param price
-	 * @return
+	 * @param name restaurant name
+	 * @param address restaurant address
+	 * @param zipCode restaurant zipcode
+	 * @param postalAddress restaurant postalAddress
+	 * @param phone restaurant phone
+	 * @param limitFreeDelivery limit for when free delivery will take place.
+	 * @return true if the value was updated, and false if not
 	 */
 	public static boolean setRestaurantProperties(String name, String address, String zipCode, 
 												  	String postalAddress, String phone, double limitFreeDelivery){
